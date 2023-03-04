cb2e1018229b0536dab32b6a03e030a4d3bc0d26
Julian Bokelmann <j.b.bokelmann+github@gmail.com>
2023-03-04 12:11:36 +0100

    ✨ add receiving data

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const client = new net.Socket();
 
 client.connect({port: 12345, host: '127.0.0.1'}, () => {
     console.log('Connected to server\n');
-    client.write('Hello, server!');
-    client.destroy()
+    client.write('Hello, server!\n');
+});
+
+client.on('data', (data) => {
+    console.log(`Received data from server: ${data}`);
 });
 
 client.on('error', (err) => {
@@ -14,4 +17,5 @@ client.on('error', (err) => {
 
 client.on('close', () => {
     console.log('Connection closed');
+    client.destroy()
 });
